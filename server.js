d9ef65abc744a8fa79f838129540aeeaba79982a
Arpit Goyal <arpit.go4@gmail.com>
2017-09-22 00:47:57 +0530

    webpack production script configured and working.

--- server.js
+++ server.js
@@ -7,3 +7,18 @@ import favicon from 'serve-favicon';
 import cors from 'cors';
 import cluster from 'cluster';
 
+const app = express();
+
+app.use('/', (req, res) => res.send('hello world!!'));
+
+function startServer() {
+    app.listen(process.env.PORT || 8080, function(err) {
+        if (err) {
+          return console.error(err);
+        }
+  
+        console.log('Listening at http://localhost:' + (process.env.PORT || 8080));
+    });
+}
+
+startServer();
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,24 @@
 
 const webpack = require('webpack');
+const path = require('path');
+
+const ENTRY_POINT = './server.js';
 
 module.exports = {
-    entry:
+    devtool: 'cheap-source-map',
+    entry: { server: ENTRY_POINT },
+    target: 'node',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].bundle.js',
+        sourceMapFilename: 'bundle.map.js',
+        publicPath: '/'
+    },
+    plugins: [],
+    module: {
+        loaders: [
+            { test: /\.html$/, loaders: ['file-loader?name=[name].[ext]'], include: path.join(__dirname, 'src') },
+            { test: /\.js$/, loaders: ['babel-loader'], include: path.join(__dirname) }
+        ]
+    }
 };
