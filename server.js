813e89c41c73e6e7cf686e0b433a14942c6f9127
Arpit Goyal <arpit.go4@gmail.com>
2017-09-22 00:11:29 +0530

    babel, esling config added. prod and dev deps added in package.json. empty webpack config file added.

---
+++ server.js
@@ -0,0 +1,9 @@
+
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import logger from 'morgan';
+import favicon from 'serve-favicon';
+import cors from 'cors';
+import cluster from 'cluster';
+
